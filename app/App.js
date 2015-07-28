1471aad4e4ca0b050c59478b7013c2fe6c90fd60
federicopede <federicopede@gmail.com>
2015-07-28 22:53:09 +0200

    Mvc + Desktop

--- app/App.js
+++ app/App.js
@@ -5,9 +5,38 @@
  * http://www.sencha.com/license
  */
 
+// requires: [
+//     'MyDesktop.controller.Users',
+//     //'MyDesktop.controller.Ctrl2'
+// ],
+
+
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
+ */
+Ext.application({
+    name: 'MyApp',
+
+    extend: 'MyDesktop.App',
+  controllers: [
+        'Users'
+    ],
+    //mainView: 'MyDesktop.App'
+
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to MyApp.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
+});
+
+
+
 Ext.define('MyDesktop.App', {
     extend: 'Ext.ux.desktop.App',
-
+ 
     requires: [
         'Ext.window.MessageBox',
 
